5def186a376847442aedf254906231632fdd9321
moonjongsul <moonjongsul@gmail.com>
2026-03-12 12:52:08 +0900

    [update] update

--- examples/LIBERO/train_files/run_libero_train_dual_node.sh
+++ examples/LIBERO/train_files/run_libero_train_dual_node.sh
@@ -3,25 +3,31 @@
 MASTER_ADDR=192.168.200.12
 MASTER_PORT=29500
 
-COMMON_ENV="
-export WANDB_MODE=disabled &&
-export PYTHONWARNINGS=ignore::UserWarning:torchvision &&
-export NCCL_SOCKET_IFNAME=enp1s0f1np1 &&
-export NCCL_IB_DISABLE=0 &&
-export NCCL_NET_GDR_LEVEL=5 &&
-export NCCL_DEBUG=WARN &&
-export TORCH_NCCL_BLOCKING_WAIT=1 &&
-export TORCH_NCCL_ASYNC_ERROR_HANDLING=1 &&
-export NCCL_TIMEOUT=3600 &&
-export TORCH_CUDA_ARCH_LIST=12.1+PTX &&
-export TORCHINDUCTOR_DISABLE=1
-"
+# 노드별 실행 스크립트 생성
+cat > /tmp/train_node.sh << 'SCRIPT'
+#!/bin/bash
+NODE_RANK=$1
 
-TRAIN_CMD="python -m torch.distributed.run \
+export WANDB_MODE=disabled
+export PYTHONWARNINGS=ignore::UserWarning:torchvision
+export NCCL_SOCKET_IFNAME=enp1s0f1np1
+export NCCL_IB_DISABLE=0
+export NCCL_NET_GDR_LEVEL=5
+export NCCL_DEBUG=WARN
+export TORCH_NCCL_BLOCKING_WAIT=1
+export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
+export NCCL_TIMEOUT=3600
+export TORCH_CUDA_ARCH_LIST=12.1+PTX
+export TORCHINDUCTOR_DISABLE=1
+
+cd /workspace/starVLA
+
+python -m torch.distributed.run \
   --nproc_per_node=1 \
   --nnodes=2 \
-  --master_addr=${MASTER_ADDR} \
-  --master_port=${MASTER_PORT} \
+  --node_rank=${NODE_RANK} \
+  --master_addr=192.168.200.12 \
+  --master_port=29500 \
   starVLA/training/train_starvla.py \
   --config_yaml ./examples/LIBERO/train_files/starvla_cotrain_libero.yaml \
   --framework.name QwenOFT \
@@ -36,22 +42,25 @@ TRAIN_CMD="python -m torch.distributed.run \
   --trainer.logging_frequency 100 \
   --trainer.eval_interval 100 \
   --run_root_dir ./results/Checkpoints \
-  --run_id 1229_libero4in1_qwen25oft"
+  --run_id 1229_libero4in1_qwen25oft
+SCRIPT
 
-NODE1_CMD="cd /workspace/starVLA && ${COMMON_ENV} && ${TRAIN_CMD} --node_rank=0"
-NODE2_CMD="cd /workspace/starVLA && ${COMMON_ENV} && ${TRAIN_CMD} --node_rank=1"
+chmod +x /tmp/train_node.sh
 
-# 노드2 컨테이너에서 worker 실행 (백그라운드)
+# 노드2에 스크립트 복사
+scp /tmp/train_node.sh js_spark@192.168.200.13:/tmp/train_node.sh
+
+# 노드2 worker 실행 (백그라운드)
 echo "[INFO] Starting worker on node2 (starVLA-node2)..."
-ssh js_spark@192.168.200.13 "docker exec starVLA-node2 bash -c '${NODE2_CMD}'" 2>&1 | sed 's/^/[node2] /' &
+ssh js_spark@192.168.200.13 "docker cp /tmp/train_node.sh starVLA-node2:/tmp/train_node.sh && docker exec starVLA-node2 bash /tmp/train_node.sh 1" 2>&1 | sed 's/^/[node2] /' &
 SSH_PID=$!
 
-# 잠시 대기 후 노드1 master 실행 (노드2가 먼저 대기 상태여야 함)
 sleep 3
 
-# 노드1 컨테이너에서 master 실행
+# 노드1 master 실행
 echo "[INFO] Starting master on node1 (starVLA-node1)..."
-docker exec starVLA-node1 bash -c "${NODE1_CMD}" 2>&1 | sed 's/^/[node1] /'
+docker cp /tmp/train_node.sh starVLA-node1:/tmp/train_node.sh
+docker exec starVLA-node1 bash /tmp/train_node.sh 0 2>&1 | sed 's/^/[node1] /'
 
 wait $SSH_PID
 echo "[INFO] Training finished."
